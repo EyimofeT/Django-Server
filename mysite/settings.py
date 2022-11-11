4ac73be7c93d9a0ab167cc5b96bff7b56e36ab93
EyimofeT <mofii4u@gmail.com>
2022-11-11 14:11:06 +0100

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,8 +29,10 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 import os
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
+import django_heroku
 
 # Application definition
 
@@ -139,3 +141,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
